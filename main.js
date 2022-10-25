e07e115dec1739177bf82d687d1a5a5dfbd613f8
BennyDagur <bdg2@nemandi.ntv.is>
2022-10-25 20:29:41 +0000

    Actually Done

--- main.js
+++ main.js
@@ -14,17 +14,28 @@ const Ron = {
     hairColor: "Red",
 }
 
-color = (c) => c
-full = (a, b) => a +" "+ b;
+const HarryHTML = document.getElementById("Harry")
+const HermoineHTML = document.getElementById("Hermoine")
+const RonHTML = document.getElementById("Ron")
 
-document.getElementById("Harry").style.color = color(Harry.hairColor)
-document.getElementById("Harry").innerHTML = full(Harry.firstName, Harry.lastName)
+color = (a, b, c) => {
+    HarryHTML.style.color = a
+    HermoineHTML.style.color = b
+    RonHTML.style.color = c
+}
+full = () => {
+    HarryHTML.innerHTML = Harry.firstName +" "+ Harry.lastName,
+    HermoineHTML.innerHTML = Hermoine.firstName +" "+ Hermoine.lastName,
+    RonHTML.innerHTML = Ron.firstName +" "+ Ron.lastName}
+first = () => {
+    HarryHTML.innerHTML = Harry.firstName
+    HermoineHTML.innerHTML = Hermoine.firstName
+    RonHTML.innerHTML = Ron.firstName
+}
 
-document.getElementById("Hermoine").style.color = color(Hermoine.hairColor)
-document.getElementById("Hermoine").innerHTML = full(Hermoine.firstName, Hermoine.lastName)
-
-document.getElementById("Ron").style.color = color(Ron.hairColor)
-document.getElementById("Ron").innerHTML = full(Ron.firstName, Ron.lastName)
+    full()
+        
+    color(Harry.hairColor, Hermoine.hairColor, Ron.hairColor)
 
 let vsTgl = 1
 document.getElementById("btn")
@@ -32,20 +43,15 @@ document.getElementById("btn")
 btn.addEventListener("click", () =>{
     
     if(vsTgl == 2){
-        document.getElementById("Harry").style.color = color(Harry.hairColor)
-        document.getElementById("Harry").innerHTML = full(Harry.firstName, Harry.lastName)
-        document.getElementById("Hermoine").style.color = color(Hermoine.hairColor)
-        document.getElementById("Hermoine").innerHTML = full(Hermoine.firstName, Hermoine.lastName)
-        document.getElementById("Ron").style.color = color(Ron.hairColor)
-        document.getElementById("Ron").innerHTML = full(Ron.firstName, Ron.lastName)
+        full()
+        
+        color(Harry.hairColor, Hermoine.hairColor, Ron.hairColor)
+
         vsTgl = 1
     } else if (vsTgl == 1){
-        document.getElementById("Harry").style.color = color("Black")
-        document.getElementById("Harry").innerHTML = Harry.firstName
-        document.getElementById("Hermoine").style.color = color("Black")
-        document.getElementById("Hermoine").innerHTML = Hermoine.firstName
-        document.getElementById("Ron").style.color = color("Black")
-        document.getElementById("Ron").innerHTML = Ron.firstName
+        first()
+        
+        color("Black", "Black", "Black")
         vsTgl = 2
     }
 })
